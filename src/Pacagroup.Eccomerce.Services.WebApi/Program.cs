06fae253f2ca642963b669536432f3b224cf9dfa
hernanjhc <hernan.jhc@gmail.com>
2023-07-22 11:38:23 -0300

    Configura Cors.-

--- src/Pacagroup.Eccomerce.Services.WebApi/Program.cs
+++ src/Pacagroup.Eccomerce.Services.WebApi/Program.cs
@@ -11,17 +11,55 @@ using Pacagroup.Eccomerce.Transversal.Common;
 using Pacagroup.Eccomerce.Transversal.Mapper;
 using Microsoft.Extensions.Configuration;
 using Pacagroup.Eccomerce.Domain.Interface;
+using System.ComponentModel;
+using Swashbuckle.Swagger;
+using System.Reflection;
+using License = Swashbuckle.Swagger.License;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+IConfiguration configuration = new ConfigurationBuilder()
+                            .AddJsonFile("appsettings.json")
+                            .Build();
 
-builder.Services.AddControllers();
+// Add services to the container.
+string myPolicy = "policyApiEcommerce";
+builder.Services.AddCors(options => options.AddPolicy(myPolicy,
+    build => build.WithOrigins(configuration["Config:OriginCors"])
+                .AllowAnyHeader()
+                .AllowAnyMethod()));
+
+builder.Services.AddCors();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-
+//c =>
+//{
+//    c.SwaggerDoc("v1", new Info
+//    {
+//        version = "v1",
+//        title = "Pacagroup Technology Services API Network",
+//        termsOfService = "None"
+//        ,
+//        contact = new Swashbuckle.Swagger.Contact
+//        {
+//            name = "Joaco Castillo",
+//            email = "hernan.jhc@gmail.com",
+//            url = ""
+//        }
+//        ,
+//        license = new Swashbuckle.Swagger.License
+//        {
+//            name = "Use under LICK"
+//            //,
+//            //url = ""
+//        }
+//    });
+//    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+//    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+//    c.IncludeXmlComments(xmlPath);
+//});
 
 builder.Services.AddAutoMapper(x => x.AddProfile(new MappingProfile()));
 
@@ -34,6 +72,7 @@ builder.Services.AddScoped<ICustomersApplication, CustomersApplication>();
 builder.Services.AddScoped<ICustomersDomain, CustomersDomain>();
 builder.Services.AddScoped<ICustomersRepository, CustomersRepository>();
 
+builder.Services.AddMvc();
 
 var app = builder.Build();
 
@@ -44,10 +83,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseCors(myPolicy);
 
 app.Run();
